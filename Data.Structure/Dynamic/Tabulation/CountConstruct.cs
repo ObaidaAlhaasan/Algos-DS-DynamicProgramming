b8990bd894ec1b9296dc2fd922e6c6f33484f54b
Obaida616 <obaida.alhassan@meccasoft.com>
2021-08-13 18:45:02 +0300

    Count Construct

--- Data.Structure/Dynamic/Tabulation/CountConstruct.cs
+++ Data.Structure/Dynamic/Tabulation/CountConstruct.cs
@@ -6,7 +6,30 @@ namespace Data.Structure.Dynamic.Tabulation
     {
         public static int G(string target, List<string> wordsBank)
         {
-            throw new System.NotImplementedException();
+            var table = new List<int?>
+            {
+                Capacity = target.Length + 1
+            };
+
+            for (var i = 0; i <= target.Length; i++)
+                table.Insert(i, 0);
+
+            table[0] = 1;
+
+            for (var i = 0; i <= target.Length; i++)
+            {
+                if (table[i] == 0)
+                    continue;
+
+                foreach (var word in wordsBank)
+                {
+                    var nextInd = i + word.Length;
+                    if (nextInd <= target.Length && word == target.Slice(i, nextInd))
+                        table[nextInd] += table[i];
+                }
+            }
+
+            return (int)(table[target.Length] == null ? 0 : table[target.Length]);
         }
     }
 }
