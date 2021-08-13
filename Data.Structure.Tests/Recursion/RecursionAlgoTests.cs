0019e75bbc07e04af9ca8489113dd036b82b79c5
Obaida616 <obaida.alhassan@meccasoft.com>
2021-08-13 20:39:27 +0300

    Recursion algo

---
+++ Data.Structure.Tests/Recursion/RecursionAlgoTests.cs
@@ -0,0 +1,27 @@
+﻿using Data.Structure.Recursion;
+using Xunit;
+
+namespace Data.Structure.Tests.Recursion
+{
+    public class RecursionAlgoTests
+    {
+        [Fact]
+        public void ReverseString()
+        {
+            Assert.Equal("olleh", RecursionAlgo.ReverseString("hello"));
+        }
+
+        [Fact]
+        public void IsPalindrome()
+        {
+            Assert.True(RecursionAlgo.IsPalindrome("kayak"));
+            Assert.False(RecursionAlgo.IsPalindrome("hello"));
+        }
+
+        [Fact]
+        public void DecimalToBinary()
+        {
+            Assert.Equal("11101001", RecursionAlgo.DecimalToBinary(233));
+        }
+    }
+}
---
+++ Data.Structure/Recursion/RecursionAlgo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Data.Structure.Recursion
+{
+    public static class RecursionAlgo
+    {
+        public static string ReverseString(string source)
+        {
+            if (source == string.Empty)
+                return string.Empty;
+
+            return ReverseString(source[1..]) + source[0];
+        }
+
+        public static bool IsPalindrome(string source)
+        {
+            if (source.Length is 0 or 1)
+                return true;
+
+            if (source[0] == source[^1])
+            {
+                return IsPalindrome(source.Slice(1, source.Length - 1));
+            }
+
+            return false;
+        }
+
+        public static string DecimalToBinary(int source, string result = "")
+        {
+            if (source == 0)
+                return result;
+
+            return DecimalToBinary(source / 2, source % 2 + result);
+        }
+    }
+}
