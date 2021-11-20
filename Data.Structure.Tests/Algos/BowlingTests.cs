4be3dd1eb86ee58237e93c6de94e4c3d29e32ae9
Obaida616 <obaida.alhassan@meccasoft.com>
2021-11-20 20:50:43 +0200

    TDD Bowling Game

---
+++ Data.Structure.Tests/Algos/BowlingTests.cs
@@ -0,0 +1,76 @@
+﻿using Data.Structure.Algos;
+using Xunit;
+
+namespace Data.Structure.Tests.Algos
+{
+    public class BowlingTests
+    {
+        private readonly Game game;
+
+        public BowlingTests()
+        {
+            game = new Game();
+        }
+
+        [Fact]
+        public void GutterGame()
+        {
+            RollMany(20, 0);
+            Assert.Equal(0, game.GetScore());
+        }
+
+        [Fact]
+        public void AllOnes()
+        {
+            RollMany(20, 1);
+            Assert.Equal(20, game.GetScore());
+        }
+
+        [Fact]
+        public void OneSpare()
+        {
+            RollSpare();
+            game.Roll(3);
+
+            RollMany(17, 0);
+
+            Assert.Equal(16, game.GetScore());
+        }
+
+        [Fact]
+        public void OneStrike()
+        {
+            RollStrike();
+            game.Roll(3);
+            game.Roll(4);
+
+            RollMany(16, 0);
+
+            Assert.Equal(24, game.GetScore());
+        }
+
+        [Fact]
+        public void PerfectGame()
+        {
+            RollMany(12, 10);
+            Assert.Equal(300, game.GetScore());
+        }
+
+
+        private void RollStrike() => game.Roll(10);
+
+
+        private void RollSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);
+        }
+
+
+        private void RollMany(int n, int pins)
+        {
+            for (var i = 0; i < n; i++)
+                game.Roll(pins);
+        }
+    }
+}
---
+++ Data.Structure/Algos/Game.cs
@@ -0,0 +1,63 @@
+﻿namespace Data.Structure.Algos
+{
+    public class Game
+    {
+        private readonly int[] rolls = new int[21];
+        private int currentRole;
+
+        public void Roll(int pins) => rolls[currentRole++] = pins;
+
+        public int GetScore()
+        {
+            var score = 0;
+            var firstInFrame = 0;
+            for (var frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(firstInFrame))
+                {
+                    score += 10 + NextTwoBallsForStrike(firstInFrame);
+                    firstInFrame++;
+                }
+                else if (IsSpare(firstInFrame))
+                {
+                    score += 10 + NextBallForSpare(firstInFrame);
+
+                    firstInFrame += 2;
+                }
+                else
+                {
+                    score += TwoBallsInFrame(firstInFrame);
+
+                    firstInFrame += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private bool IsStrike(int firstInFrame)
+        {
+            return rolls[firstInFrame] == 10;
+        }
+
+        private int TwoBallsInFrame(int firstInFrame)
+        {
+            return rolls[firstInFrame] + rolls[firstInFrame + 1];
+        }
+
+        private int NextBallForSpare(int firstInFrame)
+        {
+            return rolls[firstInFrame + 2];
+        }
+
+        private int NextTwoBallsForStrike(int firstInFrame)
+        {
+            return rolls[firstInFrame + 1] + rolls[firstInFrame + 2];
+        }
+
+        private bool IsSpare(int firstInFrame)
+        {
+            return rolls[firstInFrame] + rolls[firstInFrame + 1] == 10;
+        }
+    }
+}
