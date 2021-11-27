0c12df765392c8cf0e5f0f548a51b3ebff4b77c3
Obaida616 <obaida.alhassan@meccasoft.com>
2021-11-27 17:25:02 +0200

    Bubble Sort and Quick Sort

---
+++ Data.Structure.Tests/Algos/SortIntTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data.Structure.Algos;
+using Xunit;
+
+namespace Data.Structure.Tests.Algos
+{
+    public class SortIntTests
+    {
+        private readonly IntSorter _intSorter = new();
+
+        [Fact]
+        public async Task Sort()
+        {
+            AssertSorted(IntList(), IntList());
+            AssertSorted(IntList(1), IntList(1));
+            AssertSorted(IntList(2, 1), IntList(1, 2));
+            AssertSorted(IntList(1, 3, 2), IntList(1, 2, 3));
+            AssertSorted(IntList(1, 4, 2), IntList(1, 2, 4));
+            AssertSorted(IntList(3, 2, 1), IntList(1, 2, 3));
+
+            AssertSortedBigInt();
+        }
+
+        [Fact]
+        private void AssertSortedBigInt()
+        {
+            var n = 25000;
+            var unsorted = new List<int>();
+            var rand = new Random();
+
+            for (var i = 0; i < n; i++)
+                unsorted.Add(rand.Next(n));
+
+            var sorted = _intSorter.Sort(unsorted).ToArray();
+
+            for (int i = 0; i < sorted.Length - 1; i++)
+            {
+                Assert.True(sorted[i] <= sorted[i + 1]);
+            }
+        }
+
+        [Fact]
+        private void AssertSortedBigIntII()
+        {
+            var n = 25000;
+            var unsorted = new List<int>();
+            var rand = new Random();
+
+            for (var i = 0; i < n; i++)
+                unsorted.Add(rand.Next(n));
+
+            var sorted = _intSorter.SortII(unsorted).ToArray();
+
+            for (int i = 0; i < sorted.Length - 1; i++)
+            {
+                Assert.True(sorted[i] <= sorted[i + 1]);
+            }
+        }
+        
+
+        [Fact]
+        public async Task Sort_II()
+        {
+            AssertSortedII(IntList(), IntList());
+            AssertSortedII(IntList(1), IntList(1));
+            AssertSortedII(IntList(2, 1), IntList(1, 2));
+            AssertSortedII(IntList(2, 1, 3), IntList(1, 2, 3));
+            AssertSortedII(IntList(2, 3, 1), IntList(1, 2, 3));
+            AssertSortedII(IntList(3, 2, 1), IntList(1, 2, 3));
+            AssertSortedII(IntList(1, 3, 2), IntList(1, 2, 3));
+            AssertSortedII(IntList(3, 2, 1), IntList(1, 2, 3));
+            AssertSortedII(IntList(3, 2, 2, 1), IntList(1, 2, 2, 3));
+            
+            AssertSortedBigIntII();
+        }
+
+        private void AssertSortedII(IEnumerable<int> unsortedList, IEnumerable<int> sortedList)
+        {
+            Assert.Equal(sortedList, _intSorter.SortII(unsortedList.ToList()));
+        }
+
+        private void AssertSorted(IEnumerable<int> unsortedList, IEnumerable<int> sortedList)
+        {
+            Assert.Equal(sortedList, _intSorter.Sort(unsortedList));
+        }
+
+        private IEnumerable<int> IntList(params int[] n)
+        {
+            return new List<int>(n);
+        }
+    }
+}
---
+++ Data.Structure/Algos/IntSorter.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Data.Structure.Algos
+{
+    public class IntSorter
+    {
+        public IEnumerable<int> Sort(IEnumerable<int> originalNums)
+        {
+            var nums = originalNums.ToArray();
+
+            for (var size = nums.Length; size >= 0; size--)
+            {
+                for (var index = 0; size > index + 1; index++)
+                    if (OutOfOrder(nums, index))
+                        Swap(nums, index);
+            }
+
+            return nums;
+        }
+
+        private static bool OutOfOrder(int[] nums, int index)
+        {
+            return nums.ElementAt(index) > nums.ElementAtOrDefault(index + 1);
+        }
+
+        private static void Swap(int[] nums, int index)
+        {
+            (nums[index], nums[index + 1]) = (nums[index + 1], nums[index]);
+        }
+
+        public IEnumerable<int> SortII(IList<int> unsortedList)
+        {
+            if (unsortedList.Count == 0)
+                return unsortedList;
+
+            var sorted = new List<int>();
+            var l = new List<int>();
+            var m = unsortedList.First();
+            var h = new List<int>();
+
+            foreach (var num in unsortedList.Skip(1))
+            {
+                if (num > m)
+                    h.Add(num);
+                else
+                    l.Add(num);
+            }
+
+            sorted.AddRange(SortII(l));
+
+            sorted.Add(m);
+
+            sorted.AddRange(SortII(h));
+
+            return sorted;
+        }
+    }
+}
