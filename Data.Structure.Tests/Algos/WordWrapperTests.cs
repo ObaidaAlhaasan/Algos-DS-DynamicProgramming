4b5db6552fb174ce52fe0d9d30b07f4ba95709e2
Obaida616 <obaida.alhassan@meccasoft.com>
2021-11-27 13:36:49 +0200

    word wrapper by width algo

---
+++ Data.Structure.Tests/Algos/WordWrapperTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data.Structure.Algos;
+using Xunit;
+
+namespace Data.Structure.Tests.Algos
+{
+    public class WordWrapperTests
+    {
+        [Fact]
+        public async Task ShouldWrap()
+        {
+            AssertWraps("", 1, "");
+            AssertWraps(null, 1, "");
+            AssertWraps("x", 1, "x");
+            AssertWraps("xx", 1, "x\nx");
+            AssertWraps("xxx", 1, "x\nx\nx");
+            AssertWraps("x x", 1, "x\nx");
+            AssertWraps("x xx", 3, "x\nxx");
+            AssertWraps("x xx", 3, "x\nxx");
+            AssertWraps("four score and seven years ago our fathers brought forth upon this continent", 7, "four\nscore\nand\nseven\nyears\nago our\nfathers\nbrought\nforth\nupon\nthis\ncontine\nnt");
+        }
+
+        private static void AssertWraps(string? str, int width, string? expected)
+        {
+            Assert.Equal(expected, WordWrapper.Wrap(str, width));
+        }
+    }
+}
---
+++ Data.Structure/Algos/WordWrapper.cs
@@ -0,0 +1,20 @@
+﻿namespace Data.Structure.Algos
+{
+    public class WordWrapper
+    {
+        public static string Wrap(string text, int width)
+        {
+            if (text.IsNullOrEmpty())
+                return string.Empty;
+
+            if (text.Length <= width)
+                return text;
+
+            var breakpoint = text.LastIndexOf(' ', width);
+            if (breakpoint == -1)
+                breakpoint = width;
+
+            return text[..breakpoint] + "\n" + Wrap(text[breakpoint..].Trim(), width);
+        }
+    }
+}
