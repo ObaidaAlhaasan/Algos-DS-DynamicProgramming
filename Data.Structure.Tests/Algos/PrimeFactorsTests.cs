e7d014c3d19398490b8f850c733ed4bae6cded2a
Obaida616 <obaida.alhassan@meccasoft.com>
2021-11-27 12:54:06 +0200

    Prime Factors

---
+++ Data.Structure.Tests/Algos/PrimeFactorsTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Data.Structure.Tests.Algos
+{
+    public class PrimeFactorsTests
+    {
+        [Fact]
+        public async Task CanFactorIntoPrimes()
+        {
+            AssertPrimeFactors(ArrList(), 1);
+            AssertPrimeFactors(ArrList(2), 2);
+            AssertPrimeFactors(ArrList(3), 3);
+            AssertPrimeFactors(ArrList(2, 2), 4);
+            AssertPrimeFactors(ArrList(5), 5);
+            AssertPrimeFactors(ArrList(2, 3), 6);
+            AssertPrimeFactors(ArrList(7), 7);
+            AssertPrimeFactors(ArrList(2, 2, 2), 8);
+            AssertPrimeFactors(ArrList(3, 3), 9);
+            AssertPrimeFactors(ArrList(2, 2, 3, 3, 5, 7, 11, 11, 13), 2 * 2 * 3 * 3 * 5 * 7 * 11 * 11 * 13);
+        }
+
+        private void AssertPrimeFactors(List<int> primeFactors, int n) => Assert.Equal(primeFactors, Of(n));
+
+        private List<int> ArrList(params int[] nums) => new(nums);
+
+        private List<int> Of(int n)
+        {
+            var factors = new List<int>();
+
+            for (var divisor = 2; n > 1; divisor++)
+            {
+                for (; n % divisor == 0; n /= divisor)
+                    factors.Add(divisor);
+            }
+
+            return factors;
+        }
+    }
+}
